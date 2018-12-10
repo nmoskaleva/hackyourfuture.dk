166a1d54bea988289b2f156d495b3997292fa4b7
Zuhair Taha <admin@tahasoft.com>
2018-12-10 23:29:38 +0100

    change mentors css style

--- components/Team/index.js
+++ components/Team/index.js
@@ -1,11 +1,20 @@
 import data from './data.json'
 
+
 export default () => {
   return (
     <div className="mentors">
       {/*language=CSS*/}
       <style jsx>
         {`
+            .mentor {
+                transition: all 0.5s;
+            }
+
+            .mentor:hover {
+                background: #f7f7f7;
+            }
+
             .mentors {
                 display: flex;
                 flex-wrap: wrap;
@@ -36,14 +45,15 @@ export default () => {
                 }
 
                 .mentor .image {
-                    height: 20vw;
-                    width: 20vw;
+                    height: auto;
+                    width: 100%;
+                    display: inline-block;
                 }
             }
 
             .mentor img {
                 width: 100%;
-                filter: grayscale(100%);
+                /*filter: grayscale(100%);*/
             }
 
             .image {
@@ -52,8 +62,8 @@ export default () => {
                 max-width: 250px;
                 max-height: 250px;
                 overflow: hidden;
-                border-radius: 100%;
-                display: inline-block;
+                /*border-radius: 100%;*/
+                display: flex;
             }
 
             .mentorName {
@@ -66,47 +76,107 @@ export default () => {
             .socialIcons {
                 margin-top: 1rem;
             }
+
+            .defaultPhoto {
+                width: 150%;
+                maxWidth: 100%;
+                margin: auto;
+            }
+
+            /*shine effect*/
+            figure {
+                margin: 0;
+                padding: 0;
+                background: #fff;
+                overflow: hidden;
+            }
+
+            figure:hover + span {
+                bottom: -36px;
+                opacity: 1;
+            }
+
+            .shine figure {
+                position: relative;
+            }
+
+            .shine figure::before {
+                position: absolute;
+                top: 0;
+                left: -75%;
+                z-index: 2;
+                display: block;
+                content: '';
+                width: 50%;
+                height: 100%;
+                background: linear-gradient(to right, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, .3) 100%);
+                transform: skewX(-25deg);
+            }
+
+            .shine figure:hover::before {
+                -webkit-animation: shine .75s;
+                animation: shine .75s;
+            }
+
+            @keyframes shine {
+                100% {
+                    left: 125%;
+                }
+            }
+
+            .zoomIn1 figure img {
+                transform: scale(1);
+                transition: .23s ease-in-out;
+            }
+
+            .zoomIn1 figure:hover img {
+                transform: scale(1.2);
+                filter: grayscale(100%)
+            }
         `}
       </style>
-      {data.sort((a, b) => a.name.localeCompare(b.name)).map((item, key) => {
-        return (
-          <div className={'mentor'} key={key}>
-            <div className="image">
-              {item.photo ? (
-                <img src={item.photo} />
-              ) : (
-                <img
-                  src={'/static/hackyourfuture.png'}
-                  style={{width: '150%', maxWidth: '150%'}}
-                />
+      {data.sort((a, b) => a.name.localeCompare(b.name)).map((item, key) =>
+        <div className='mentor' key={key}>
+          <div className="image">
+            {item.photo
+              ? <div className="shine zoomIn1">
+                <figure>
+                  <a href={item.github}>
+                    <img src={item.photo} />
+                  </a>
+                </figure>
+              </div>
+              : <img
+                className="defaultPhoto"
+                src={'/static/HYF_LOGO.png'}
+              />
+            }
+          </div>
+          <h3 className="mentorName">
+            {item.name}
+            <div className="socialIcons">
+              {item.github && (
+                <a href={item.github}>
+                  <img
+                    style={{width: 33, padding: 3, opacity: 0.5}}
+                    src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg"
+                    alt="Github"
+                  />
+                </a>
+              )}
+              {item.linkedin && (
+                <a href={item.linkedin}>
+                  <img
+                    style={{width: 33, padding: 3, opacity: 0.5}}
+                    src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/linkedin.svg"
+                    alt="Linkedin"
+                  />
+                </a>
               )}
             </div>
-            <h3 className="mentorName">
-              {item.name}
-              <div className="socialIcons">
-                {item.github && (
-                  <a href={item.github}>
-                    <img
-                      style={{width: 33, padding: 3, opacity: 0.5}}
-                      src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/github.svg"
-                      alt="Github"
-                    />
-                  </a>
-                )}
-                {item.linkedin && (
-                  <a href={item.linkedin}>
-                    <img
-                      style={{width: 33, padding: 3, opacity: 0.5}}
-                      src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/linkedin.svg"
-                      alt="Linkedin"
-                    />
-                  </a>
-                )}
-              </div>
-            </h3>
-          </div>
-        )
-      })}
+          </h3>
+        </div>
+      )}
     </div>
   )
 }
