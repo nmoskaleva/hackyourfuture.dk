9f99293680840b8ac5a92b5b4bd4b2ae1cb41aa9
Zuhair Taha <admin@tahasoft.com>
2019-01-15 02:59:34 +0100

    set image to 100%

--- pages/apply.js
+++ pages/apply.js
@@ -3,58 +3,58 @@ import marked from 'marked'
 import Layout from '../components/layouts/layout'
 import Content from '../components/layouts/content'
 import Apply from '../components/apply/index'
-import { content, fill, title } from '../components/content/_apply'
-import {
-  content as content_learn,
-  title as title_learn
-} from '../components/content/_learn'
+import { content, title } from '../components/content/_apply'
+import { content as content_learn, title as title_learn } from '../components/content/_learn'
 
 export default () => (
   <Layout>
-    {/*language=CSS*/}
+    {/*language=CSS*/ }
     <style jsx>
-      {`
-        .image {
-          position: relative;
-          left: -20px;
-          right: -20px;
-          width: calc(100% + 40px);
-          max-width: none;
-        }
-      `}
+      { `
+          .image {
+              position: relative;
+              left: -20px;
+              right: -20px;
+              width: calc(100% + 40px);
+              max-width: none;
+          }
+      ` }
     </style>
     <Head>
-      <title>{title}</title>
+      <title>{ title }</title>
     </Head>
 
-    <Content id="apply">
-      <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
-      <img className="image" src="https://i.imgur.com/ukKK09t.jpg" />
+    <Content id='apply'>
+      <div dangerouslySetInnerHTML={ {__html: marked(content)} }/>
+      <img
+        style={ {maxWidth: '100%'} }
+        className='image'
+        alt='Apply to be a student'
+        src='https://i.imgur.com/ukKK09t.jpg'
+      />
     </Content>
 
-    <Content id="apply-for-class">
+    <Content id='apply-for-class'>
       <h2>Apply for class</h2>
-      <Apply email={'cph@hackyourfuture.net'} />
+      <Apply email={ 'cph@hackyourfuture.net' }/>
     </Content>
 
-
-
-    <Content id="coding-test">
+    <Content id='coding-test'>
       <h2>Coding Tester Class for Women</h2>
       <div
-        dangerouslySetInnerHTML={{
+        dangerouslySetInnerHTML={ {
           __html: marked(
             `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad aut beatae consectetur
             cupiditate eaque molestias officiis omnis possimus quaerat? Accusantium aperiam assumenda cumque
             ipsam laboriosam maxime officiis quisquam ullam.`
           )
-        }}
+        } }
       />
     </Content>
 
-    <Content id="learn">
-      <h2>{title_learn}</h2>
-      <div dangerouslySetInnerHTML={{ __html: marked(content_learn) }} />
+    <Content id='learn'>
+      <h2>{ title_learn }</h2>
+      <div dangerouslySetInnerHTML={ {__html: marked(content_learn)} }/>
     </Content>
   </Layout>
 )
