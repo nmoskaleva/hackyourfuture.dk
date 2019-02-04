3441d5b001c95bd41257b1995754c1c465012749
Zuhair Taha <admin@tahasoft.com>
2019-02-04 13:30:33 +0100

    add scss

--- components/parallax/index.js
+++ components/parallax/index.js
@@ -1,37 +1,11 @@
 import React from 'react'
+import './parallax.scss'
 
 const MyParallax = _ => (
-  <div className="container">
-    {/*language=CSS*/ }
-    <style jsx>
-      { `
-          .container {
-              display: flex;
-              flex-direction: column;
-              height: 60vh;
-              background: fixed url('/static/background.jpg') no-repeat center center/cover;
-              alin-items: center;
-              jsutify-content: center;
-              text-align: center;
-              position: relative;
-              left: 0;
-              right: 0;
-              top: 0;
-              z-index: -1;
-          }
-
-          .logo {
-              max-height: 50vh;
-              max-width: 50vh;
-              display: block;
-              margin: auto;
-              opacity: 0.85;
-          }
-      ` }
-    </style>
-    {/*<img className="logo" src="/static/HYF_LOGO.png"/>*/ }
-    <div className="logo">
-      <img src="/static/logo_white.svg" alt="HackYourFuture Copenhagen"/>
+  <div className='parallax container'>
+    {/*<img className="logo" src="/static/HYF_LOGO.png"/>*/}
+    <div className='logo'>
+      <img src='/static/logo_white.svg' alt='HackYourFuture Copenhagen' />
     </div>
   </div>
 )
