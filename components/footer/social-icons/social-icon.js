fbadedfbad175ed3c19318b64de2af06c2173fd1
Zuhair Taha <admin@tahasoft.com>
2019-02-25 11:55:37 +0100

    remove material from social-icons component

--- components/footer/social-icons/social-icon.js
+++ components/footer/social-icons/social-icon.js
@@ -1,43 +1,38 @@
 import React from 'react'
-import IconButton from '@material-ui/core/IconButton'
 import { Facebook, GithubCircle, Linkedin, Twitter } from 'mdi-material-ui'
 import styles from './social-icon.scss'
 
 const SocialIcons = () => (
   <div className='social-icons'>
     <style jsx>{styles}</style>
-    <IconButton
+    <a
       href='https://www.facebook.com/hackyourfuturecopenhagen/'
-      color='inherit'
       title='facebook'
       target='_blank'
     >
       <Facebook />
-    </IconButton>
-    <IconButton
+    </a>
+    <a
       href='https://twitter.com/HackyourfutureC'
-      color='inherit'
       title='twitter'
       target='_blank'
     >
       <Twitter />
-    </IconButton>
-    <IconButton
+    </a>
+    <a
       href='https://www.linkedin.com/school/hackyourfuture-copenhagen/'
-      color='inherit'
       title='linkedin'
       target='_blank'
     >
       <Linkedin />
-    </IconButton>
-    <IconButton
+    </a>
+    <a
       href='https://github.com/hackyourfuture-cph'
-      color='inherit'
       title='github'
       target='_blank'
     >
       <GithubCircle />
-    </IconButton>
+    </a>
   </div>
 )
 
