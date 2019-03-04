19c089bf625a13887381f9197d9cc71ad846d6df
behuFTP <benjamin.dals.hughes@gmail.com>
2019-03-04 16:13:14 +0100

    Add two node environment names
    One for next js to use: NODE_ENV
    The other for custom environment checking: NODE_ENVIRONMENT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const next = require('next')
 var enforce = require('express-sslify');
 
 const port = parseInt(process.env.PORT, 10) || 3000
+// NODE_ENV is used for next.js ONLY
+// NODE_ENVIRONMENT is used for custom environment checking
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -10,7 +12,7 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENVIRONMENT === 'production') {
     server.use(enforce.HTTPS({ trustProtoHeader: true }));
   }
 
