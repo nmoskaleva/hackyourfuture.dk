d35d0fb5108ba58e46500c9cf5c80b7eaae41e8a
behuFTP <benjamin.dals.hughes@gmail.com>
2019-03-05 14:54:55 +0100

    Add zapier as partner

--- components/partners/partners.js
+++ components/partners/partners.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import partners from './partners.json'
 import styles from './partners.scss'
+console.log(4);
+
 
 export default () => (
   <div className='partners'>
     <style jsx>{styles}</style>
     <h2 className='center'>Official partners</h2>
     <div className='wrapper'>
-      {partners.map(({ id, logo, url, title }) => (
+      {partners.map(({ id, logo, url, title, width }) => (
         <div className='partner' key={id}>
           <a rel='noopener' target='_blank' href={url}>
-            <img alt={title} src={`/static/partners/${logo}`} />
+            <img alt={title} src={`/static/partners/${logo}`} width={width ? width: ''} />
           </a>
         </div>
       ))}
