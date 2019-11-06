b3b38fb8e54c8b688d68cf09b3fd745214170554
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-11-06 14:07:09 +0100

    faq component is improved ;)

--- components/faq/faq.js
+++ components/faq/faq.js
@@ -2,12 +2,31 @@ import React from 'react'
 import Questioning from './questioning-card'
 import questions from './questions'
 
+// imports material UI
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+// styling
+const useStyles = makeStyles(() => ({
+  title: {
+    color: '#293a7d',
+    fontFamily: 'Space Mono, monospace',
+    fontSize: '2rem',
+    margin: '10px 0 40px 0'
+  }
+}))
+
 export default function faq() {
+  const classes = useStyles()
   return (
-    <React.Fragment>
+    <Container>
+      <Typography className={classes.title}>
+        Frequently Asked Questions
+      </Typography>
       {questions.map(({ id, question, answer }) => (
         <Questioning key={id} question={question} answer={answer} />
       ))}
-    </React.Fragment>
+    </Container>
   )
 }
