4027fbe4dd9d3a6ae0db618a6d6bb44ec299b350
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-11-08 00:09:33 +0100

    Learning Section is created :)

---
+++ components/learning-section/content.js
@@ -0,0 +1,5 @@
+const title = "What you'll learn"
+const description =
+  'We will teach you the fundamentals of full-stack web development, with a focus on modern Javascript. You will also learn to work with state-of-the-art tools and frameworks, receive soft-skill training and learn how to work collaboratively in a modern development team.'
+
+export { title, description }
