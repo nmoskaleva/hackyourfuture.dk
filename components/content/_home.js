b6a23e0909a53ff5fa1112dea5d7c391ae96adc7
Christopher Klueter <ch.klueter@gmail.com>
2019-12-10 21:40:33 +0100

    one more

--- components/content/_home.js
+++ components/content/_home.js
@@ -1,5 +1,7 @@
 const title = `HackYourFuture Copenhagen - A non-profit coding school for disadvantaged groups`
 const content = `
+## What is HackYourFuture?
+
 <span class="hyf-title">HackYourFuture</span> supports refugees, asylum seekers and disadvantaged groups with limited access to further education and the Danish labour market in acquiring the necessary skills to become web developers and entering a very in-demand field. Our goal is not only to educate our students, but also to guide them towards employment and integrate them into the developer community.
 
 In our 32 week program we train students with and without a coding background in modern JavaScript software development. Our aim is to have our students land their first software development job after finishing our course and we prepare and support them along the way. We have created a curriculum catering specifically to newcomers in the world of coding which we continuously adapt to market demands. Our teachers are volunteers and working professionals who are extremely passionate about coding, helping others and growing through the experience of teaching. 
