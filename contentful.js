0f554b122b639e6101f5463af84b101d423d6387
nmoskaleva <nsmoskaleva@gmail.com>
2020-03-27 22:30:25 -0700

    add env variables

--- contentful.js
+++ contentful.js
@@ -1,8 +1,8 @@
-//require('dotenv').config();
+require('dotenv').config();
 
 import { createClient } from 'contentful'
 
 export default createClient({
-    space: "x5wo3czckq34",
-    accessToken: "7t1IR7IpJnnHXuDXYQe24a9U2dxaPemmIUyees0jfXE"
+    space: process.env.CTF_SPACE_ID,
+    accessToken: process.env.CTF_ACCESS_TOKEN
 })
