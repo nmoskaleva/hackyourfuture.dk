93fb0870b3d253230e622370441866019632c87b
nmoskaleva <nsmoskaleva@gmail.com>
2020-03-27 21:59:27 -0700

    add Contentful client

---
+++ contentful.js
@@ -0,0 +1,8 @@
+//require('dotenv').config();
+
+import { createClient } from 'contentful'
+
+export default createClient({
+    space: "x5wo3czckq34",
+    accessToken: "7t1IR7IpJnnHXuDXYQe24a9U2dxaPemmIUyees0jfXE"
+})
