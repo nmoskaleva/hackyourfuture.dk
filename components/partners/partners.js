41dfb8f077cc2c250f946715e8a8f8834e58d992
nmoskaleva <nsmoskaleva@gmail.com>
2020-03-27 22:19:03 -0700

    press and partners – from Contentful

--- components/partners/partners.js
+++ components/partners/partners.js
@@ -1,39 +1,77 @@
 import React from 'react'
-import partners from './partners.json'
+import { useEffect, useState } from 'react'
 import styles from './partners.scss'
 
-export default () => (
-  <>
-    <style jsx>{styles}</style>
-    <section className='partners'>
-      <h2 className='center'>Sponsored by</h2>
-      <a
-        aria-label='Partner link'
-        rel='noopener'
-        target='_blank'
-        href='https://www.apmollerfonde.dk/'
-        className='mollerske'
-      >
-        <img
-          alt='Den A.P. Møllerske Støttefond logo'
-          src={`/static/partners/mollerske.png`}
-        />
-      </a>
-      <h2 className='center'>Supported by</h2>
-      <div className='wrapper'>
-        {partners.map(({ id, logo, url, title, width }) => (
-          <div className='partner' key={id}>
-            <a
-              aria-label='Partner link'
-              rel='noopener'
-              target='_blank'
-              href={url}
-            >
-              <img alt={title} src={`/static/partners/${logo}`} width='150vw' />
-            </a>
-          </div>
-        ))}
-      </div>
-    </section>
-  </>
-)
+// Contentful client
+import client from '../../contentful'
+
+// Contentful entry ids
+const partnersEntryId = '3CEyXyD8onF9pJd1cMboxP'
+const sponsorEntryId = '3H7wmN35ixihm684bqeSea'
+
+export default () => {
+  async function fetchEntry(entryId) {
+    const entry = await client.getEntry(entryId)
+    const entryData = entry.fields
+    if (entry.fields) return entryData
+    else console.log(`error`)
+  }
+
+  const fetchPartnersData = fetchEntry(partnersEntryId)
+  const fetchSponsorData = fetchEntry(sponsorEntryId)
+
+  const [partners, setPartners] = useState({ partnersList: [] })
+  const [sponsor, setSponsor] = useState({ title: '', logo: '', url: '' })
+
+  useEffect(() => {
+    async function getPartners() {
+      const partnersData = await fetchPartnersData
+      setPartners({ partnersList: [...partnersData.list] })
+    }
+    getPartners()
+  }, [])
+
+  useEffect(() => {
+    async function getSponsor() {
+      const sponsorData = await fetchSponsorData
+      setSponsor({ title: sponsorData.title, logo: sponsorData.logo.fields.file.url, url: sponsorData.url })
+    }
+    getSponsor()
+  }, [])
+
+  return (
+    <>
+      <style jsx>{styles}</style>
+      <section className='partners'>
+        <h2 className='center'>Sponsored by</h2>
+        <a
+          aria-label='Partner link'
+          rel='noopener'
+          target='_blank'
+          href={sponsor.url}
+          className='mollerske'
+        >
+          <img
+            alt='Den A.P. Møllerske Støttefond logo'
+            src={sponsor.logo}
+          />
+        </a>
+        <h2 className='center'>Supported by</h2>
+        <div className='wrapper'>
+          {partners.partnersList.map(partner => (
+            <div className='partner' key={partner.fields.id}>
+              <a
+                aria-label='Partner link'
+                rel='noopener'
+                target='_blank'
+                href={partner.fields.url}
+              >
+                <img alt={partner.fields.title} src={partner.fields.logo.fields.file.url} width='150vw' />
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
--- components/partners/press.js
+++ components/partners/press.js
@@ -1,19 +1,44 @@
 import React from 'react'
-import press from './press.json'
+import { useEffect, useState } from 'react'
 import styles from './partners.scss'
 
-export default () => (
-  <div className='partners'>
-    <style jsx>{styles}</style>
-    <h2 className='center'>Featured in</h2>
-    <div className='wrapper'>
-      {press.map(({ id, logo, url, title }) => (
-        <div className='partner' key={id}>
-          <a aria-label='Media link' rel='noopener' target='_blank' href={url}>
-            <img alt={title} src={`/static/media/${logo}`} width='170vw' />
-          </a>
-        </div>
-      ))}
+// Contentful client
+import client from '../../contentful'
+
+// Contentful entry id
+const pressEntryId = '3EdwVQtZsN7jEnaxwe9qbm'
+
+export default () => {
+  async function fetchEntry() {
+    const entry = await client.getEntry(pressEntryId)
+    const press = entry.fields.list
+    if (entry.fields) return press
+    else console.log(`error`)
+  }
+
+  const [press, setPress] = useState([])
+
+  useEffect(() => {
+    async function getPress() {
+      const pressData = await fetchEntry()
+      setPress([...pressData])
+    }
+    getPress()
+  }, [])
+
+  return (
+    <div className='partners'>
+      <style jsx>{styles}</style>
+      <h2 className='center'>Featured in</h2>
+      <div className='wrapper'>
+        {press.map(press => (
+          <div className='partner' key={press.fields.id}>
+            <a aria-label='Media link' rel='noopener' target='_blank' href={press.fields.url}>
+              <img alt={press.fields.title} src={press.fields.logo.fields.file.url} width='170vw' />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
