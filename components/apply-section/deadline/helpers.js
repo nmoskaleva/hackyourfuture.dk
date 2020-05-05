8227f70f994f151a9f5047da457bacc7a3098237
behuFTP <benjamin.dals.hughes@gmail.com>
2020-05-05 16:12:51 +0200

    Fixed small problem with countdown days

--- components/apply-section/deadline/helpers.js
+++ components/apply-section/deadline/helpers.js
@@ -5,7 +5,7 @@ const getTimeToDeadline = date => {
 
   if (difference > 0) {
     timeLeftInUnits = {
-      days: ('0' + Math.floor(difference / (1000 * 60 * 60 * 24))).slice(-2),
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       hours: ('0' + Math.floor((difference / (1000 * 60 * 60)) % 24)).slice(-2),
       min: ('0' + Math.floor((difference / 1000 / 60) % 60)).slice(-2),
       sec: ('0' + Math.floor((difference / 1000) % 60)).slice(-2)
