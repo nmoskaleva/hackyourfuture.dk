f257f9ba4adc9e144ca9f4b0a8212ae6025512b4
behuFTP <benjamin.dals.hughes@gmail.com>
2020-09-22 13:28:17 +0200

    fixed weird font size

--- pages/donate.js
+++ pages/donate.js
@@ -16,7 +16,8 @@ export default () => (
       <style jsx>
         {`
           li {
-              list-style: inside;
+            font-size: 18px;
+            list-style: inside;
         `}
       </style>
       <h2 id='member'>Become a company member of HackYourFuture! </h2>
@@ -36,14 +37,16 @@ export default () => (
       </ul>
       <div>
         <h3>If you answered yes</h3>
-        Then you now have a <strong>great opportunity</strong> to become a
-        company member of HackYourFuture. Thereby{' '}
-        <strong>
-          supporting the next generation of talented web-developers.
-        </strong>
+        <p>
+          Then you now have a <strong>great opportunity</strong> to become a
+          company member of HackYourFuture. Thereby{' '}
+          <strong>
+            supporting the next generation of talented web-developers.
+          </strong>
+        </p>
         <br />
-        <br />
-        At the same time your company gain membership benefits like;
+        <p>At the same time your company gain membership benefits like;</p>
+
         <ul>
           <li>First hand access to our top talents</li>
           <li>Publicity</li>
@@ -53,11 +56,13 @@ export default () => (
           <li>Possibility to arrange events with HackYourFuture</li>
         </ul>
         <br />
-        If <strong>this seems interesting</strong> for your company then get in
-        touch here:
-        <br />
-        <br />
-        <a href='mailto:cph@hackyourfuture.dk'>cph@hackyourfuture.dk</a>
+        <p>
+          If <strong>this seems interesting</strong> for your company then get
+          in touch here:
+          <br />
+          <br />
+          <a href='mailto:cph@hackyourfuture.dk'>cph@hackyourfuture.dk</a>
+        </p>
       </div>
       {/* "Gold partners" > 100.000 kr
 "Silver partners" > 30.000 kr
